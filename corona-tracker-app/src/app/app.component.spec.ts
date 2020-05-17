12229714d08941a11690321e9d8d5b8bfa0a7322
Edwin van der Ven <edwinvanderven@gmail.com>
2020-05-17 12:03:28 +0200

    corona-tracker-app: fixed the failing unit tests

--- corona-tracker-app/src/app/app.component.spec.ts
+++ corona-tracker-app/src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'corona-tracker-app'`, () => {
+  it(`should have as title 'Corona Tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('corona-tracker-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('corona-tracker-app app is running!');
+    expect(app.title).toEqual('Corona Tracker');
   });
 });
--- corona-tracker-app/src/app/corona.service.spec.ts
+++ corona-tracker-app/src/app/corona.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CoronaService } from './corona.service';
 
@@ -6,7 +7,9 @@ describe('CoronaService', () => {
   let service: CoronaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(CoronaService);
   });
 
