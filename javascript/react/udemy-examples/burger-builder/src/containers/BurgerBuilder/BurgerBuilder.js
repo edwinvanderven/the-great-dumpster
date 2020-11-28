29a3c150fbb72af2621ff60fee99a5634d6c61b2
Edwin van der Ven <edwinvanderven@gmail.com>
2020-11-28 17:41:44 +0100

    Added Routing and an additional Orders page to the application

--- javascript/react/udemy-examples/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ javascript/react/udemy-examples/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,6 +31,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
+        console.log(this.props);
         axios.get('https://react-my-burger-125e0.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data});
@@ -93,38 +94,17 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({
-            loading: true
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            const value = encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]);
+            queryParams.push(value);
+        }
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
         });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice, // <- not really safe
-            customer: {
-                name: 'Mad Max',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '1234AB',
-                    country: 'Timboektoe'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        };
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                });
-                console.log(response)
-            })
-            .catch(error => {
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                });
-                console.log(error)
-            });
     }
 
     render () {
