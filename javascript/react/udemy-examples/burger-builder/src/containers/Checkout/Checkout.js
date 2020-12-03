4f8a2fff540b76adab94e235f95d04ade34de68b
Edwin van der Ven <edwinvanderven@gmail.com>
2020-12-03 12:01:20 +0100

    Added async calls with Redux to retrieve information from Firebase

--- javascript/react/udemy-examples/burger-builder/src/containers/Checkout/Checkout.js
+++ javascript/react/udemy-examples/burger-builder/src/containers/Checkout/Checkout.js
@@ -1,12 +1,15 @@
 import React, { Component} from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
-  
+  componentDidMount() {
+    // this.props.onInitPurchase();
+  }
+
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
   }
@@ -16,24 +19,37 @@ class Checkout extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <CheckoutSummary 
-          ingredients={this.props.ings}
-          checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedHandler}/>
-        <Route 
-          path={this.props.match.path + '/contact-data'} 
-          component={ContactData} />
-      </div>
-    );
+    let summary = <Redirect to="/"/>
+    if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/"/> : null;
+      summary = (
+        <div>
+          {purchasedRedirect}
+          <CheckoutSummary 
+            ingredients={this.props.ings}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler}/>
+          <Route 
+            path={this.props.match.path + '/contact-data'} 
+            component={ContactData} />
+        </div>
+      );
+    }
+    return summary;
   }
 }
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
   };
 };
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     onInitPurchase: () => dispatch(actions.purchaseInit())
+//   };
+// };
 
 export default connect(mapStateToProps)(Checkout);
--- javascript/react/udemy-examples/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ javascript/react/udemy-examples/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -7,7 +7,8 @@ import classes from './ContactData.css';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
-
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 class ContactData extends Component {
   state = {
     orderForm: {
@@ -97,7 +98,6 @@ class ContactData extends Component {
 
   orderHandler = (event) => {
     event.preventDefault();
-    this.setState({loading: true});
     const formData = {};
     for (let formElementIdentifier in this.state.orderForm) {
       formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
@@ -107,16 +107,7 @@ class ContactData extends Component {
         price: this.props.price.toFixed(2),
         orderData: formData
     };
-    axios.post('/orders.json', order)
-        .then(response => {
-            this.setState({loading: false});
-            console.log(response);
-            this.props.history.push('/');
-        })
-        .catch(error => {
-            this.setState({loading: false});
-            console.log(error);
-        });
+    this.props.onOrderBurger(order);
   }
 
   checkValidity(value, rules) {
@@ -181,7 +172,7 @@ class ContactData extends Component {
         <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />
     }
     return (
@@ -195,9 +186,16 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    loading: state.order.loading
   };
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+  return {
+    onOrderBurger: (order) => dispatch(actions.purchaseBurger(order))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
